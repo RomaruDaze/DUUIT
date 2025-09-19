b4d47afff92b13356674b1b1cf6663abdca7653c
RomaruDaze <20122077@kaishi-pu.ac.jp>
2025-09-19 22:54:02 +0700

    Remove unused Firebase analytics import to streamline the Firebase configuration.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
@@ -19,7 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Export the required services
 export const auth = getAuth(app);
